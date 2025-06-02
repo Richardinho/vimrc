aaf3adeb9d667dde1ed4d324874f8c2091bbcf81
Richard Hunter <richard.hunter@hearst.co.uk>
2025-06-02 09:11:56 +0100

    shortcuts for fugitive

--- jakob/lua/plugins/fugitive.lua
+++ jakob/lua/plugins/fugitive.lua
@@ -1,3 +1,20 @@
 return {
 	"tpope/vim-fugitive",
+	keys = {
+		{
+			"<leader>gw",
+			"<cmd>Gwrite<CR>",
+			desc = "Git write",
+		},
+		{
+			"<leader>gs",
+			"<cmd>Git<CR>",
+			desc = "Git status",
+		},
+		{
+			"<leader>gb",
+			"<cmd>Git blame<CR>",
+			desc = "Git blame",
+		},
+	},
 }
