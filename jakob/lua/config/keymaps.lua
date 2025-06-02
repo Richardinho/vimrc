cb2e2d8246013b214b40a9c9da9d122cb630cc7a
Richard Hunter <richard.hunter@hearst.co.uk>
2025-06-02 08:59:01 +0100

    nerdtree and fugitive

--- jakob/lua/config/keymaps.lua
+++ jakob/lua/config/keymaps.lua
@@ -1,3 +1,7 @@
-vim.keymap.set("n", "-", "<cmd>Oil --float<CR>", { desc='open Oil' })
+vim.keymap.set("n", "-", "<cmd>Oil --float<CR>", { desc = "open Oil" })
 
-vim.keymap.set("n", "gl", function() vim.diagnostic.open_float() end, { desc = "open diagnostics in float" })
+vim.keymap.set("n", "gl", function()
+	vim.diagnostic.open_float()
+end, { desc = "open diagnostics in float" })
+
+vim.keymap.set("n", "<leader>q", "<cmd>q<CR>", { desc = "close window" })
---
+++ jakob/lua/plugins/nerdtree.lua
@@ -0,0 +1,9 @@
+return {
+	"preservim/nerdtree",
+	keys = {
+		{ "<leader>tr", "<cmd>NERDTree<cr>", desc = "open NERDTree" },
+		{ "<leader>tf", "<cmd>NERDTreeFocus<cr>", desc = "focus NERDTree" },
+		{ "<leader>tv", "<cmd>NERDTreeFind<cr>", desc = "find NERDTree" },
+		{ "<leader>tc", "<cmd>NERDTreeClose<cr>", desc = "close NERDTree" },
+	},
+}
